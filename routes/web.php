af3b3f8c1b4bbeec403d39b14783294d39927bd1
ivananjay <bossgreengaming@gmail.com>
2023-08-29 15:22:08 +0700

    mengupload file

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login.login');
+});
+
+Route::get('/register', function () {
+    return view('register.register');
+});
